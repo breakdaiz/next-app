160ad226bd32c9e8ed1ba6d9c62db49054121459
bernard rayoso <rayosobernard@gmail.com>
2024-05-14 00:20:51 +0800

    New Page

---
+++ app/users/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <div>
+      <ul>
+        {users.map(user => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersPage;
